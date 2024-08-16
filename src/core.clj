f83efe9ef32f3f89ad21ebd5d214f500619d980b
Kyle Grierson <griersonky@gmail.com>
2024-08-16 20:13:57 +0100

    Reading events

--- src/core.clj
+++ src/core.clj
@@ -1,4 +1,31 @@
-(ns core)
+(ns core
+  (:require
+   [aleph.http :as http]
+   [manifold.stream :as s]
+   [jsonista.core :as j]
+   [hashp.core]))
 
-(defn foo [x] x)
+(def client
+  (try
+    (http/websocket-client "wss://purplepag.es")
+    (catch Exception e (println e))))
 
+(def fetch-request
+  (j/write-value-as-string ["REQ" "subid" {:kinds [1] :limit 10}]))
+
+(defmulti read-event (fn [x]
+                       (let [[type & _] (j/read-value x)]
+                         type)))
+
+(defmethod read-event "EVENT" [params]
+  (println params))
+
+(defmethod read-event "EOSE" [params]
+  (println params))
+
+(defmethod read-event :default [params]
+  (println params))
+
+(comment
+  (s/try-put! @client fetch-request 1000 :timeout)
+  (j/rea) @(s/take! @client))
