f4eeaa24a79a2053193dd27d681b93f10a9ed955
Kyle Grierson <griersonky@gmail.com>
2024-08-16 21:28:18 +0100

    update deps

--- src/core.clj
+++ src/core.clj
@@ -13,19 +13,25 @@
 (def fetch-request
   (j/write-value-as-string ["REQ" "subid" {:kinds [1] :limit 10}]))
 
-(defmulti read-event (fn [x]
-                       (let [[type & _] (j/read-value x)]
-                         type)))
+(defmulti read-event
+  (fn [x]
+    (let [[type & _] (j/read-value x)]
+      type)))
 
-(defmethod read-event "EVENT" [params]
-  (println params))
+(defmethod read-event "EVENT" [raw-event]
+  (println "event")
+
+  (let [[_type _subscription-id body] (j/read-value raw-event j/keyword-keys-object-mapper)]
+    (println body)))
 
 (defmethod read-event "EOSE" [params]
+  (println "eose")
   (println params))
 
 (defmethod read-event :default [params]
+  (println "default")
   (println params))
 
 (comment
-  (s/try-put! @client fetch-request 1000 :timeout)
-  (j/rea) @(s/take! @client))
+  (s/put! @client fetch-request)
+  (read-event @(s/take! @client)))
