3414224bd46628d85e5413b93a7f76a675f82dc4
Kyle Grierson <griersonky@gmail.com>
2024-08-21 20:06:39 +0100

    TEMP: Read events test

--- src/nostril/core.clj
+++ src/nostril/core.clj
@@ -1,9 +1,44 @@
-(ns core
+(ns nostril.core
   (:require
    [aleph.http :as http]
    [manifold.stream :as s]
-   [jsonista.core :as j]
-   [hashp.core]))
+   [jsonista.core :as json]
+   [hashp.core]
+   [malli.generator :as mg]))
+
+(def hex-32 [:string {:min 64 :max 64}])
+(def hex-64 [:string {:min 128 :max 128}])
+(def unix-timestamp
+  [:and int? [:fn {:error/message "must be a 10-digit number"} #(<= 1000000000 % 9999999999)]])
+(def Kind [:int {:min 0 :max 65535}])
+
+(def TagE [:catn
+           [:type [:= "e"]]
+           [:event-id hex-32]
+           [:relay-url [:? uri?]]])
+
+(def TagP [:catn
+           [:type [:= "p"]]
+           [:event-id hex-32]
+           [:relay-url [:? uri?]]])
+
+(def TagA [:catn
+           [:type [:= "a"]]
+           [:event-rn hex-32]
+           [:relay-url [:? string?]]])
+
+(def Event
+  [:map
+   [:id hex-32]
+   [:pubkey hex-32]
+   [:created_at unix-timestamp]
+   [:kind Kind]
+   [:tags [:sequential [:alt TagA TagE TagP]]]
+   [:content string?]
+   [:sig hex-64]])
+
+(comment
+  (mg/generate Event))
 
 (def client
   (try
@@ -11,17 +46,16 @@
     (catch Exception e (println e))))
 
 (def fetch-request
-  (j/write-value-as-string ["REQ" "subid" {:kinds [1] :limit 10}]))
+  (json/write-value-as-string
+   ["REQ" "subid" {:kinds [1] :limit 10}]))
 
 (defmulti read-event
   (fn [x]
-    (let [[type & _] (j/read-value x)]
-      (print "Type: " type)
+    (let [[type & _] (json/read-value x)]
       type)))
 
 (defmethod read-event "EVENT" [raw-event]
-  (let [[_type _subscription-id body] (j/read-value raw-event j/keyword-keys-object-mapper)]
-    (println body)))
+  (json/read-value #p raw-event json/keyword-keys-object-mapper))
 
 (defmethod read-event "EOSE" [params]
   (println "eose")
--- test/nostril/core_test.clj
+++ test/nostril/core_test.clj
@@ -1,3 +1,19 @@
 (ns nostril.core-test
   (:require [clojure.test :refer [deftest is testing]]
-            [nostril.core :as core]))
+            [nostril.core :as core]
+            [malli.generator :as mg]
+            [tick.core :as t]
+            [jsonista.core :as json]
+            [hashp.core]))
+
+(defn now-unix []
+  (-> (t/now)
+      (t/instant)
+      (.getEpochSecond)))
+
+(deftest read-event-test
+  (testing "reading EVENT event"
+    (let [event (mg/generate core/Event)
+          response (json/write-value-as-string ["EVENT" "subid" event])
+          [_type _subid res-event] (core/read-event response)]
+      (is (= #p res-event event)))))
