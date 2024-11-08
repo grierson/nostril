e3d8421b97e12f7c3eff735d933002df9b7b2043
Kyle Grierson <griersonky@gmail.com>
2024-11-08 08:35:30 +0000

    Add each page

--- src/nostril/core.clj
+++ src/nostril/core.clj
@@ -31,13 +31,59 @@
 (defonce *content (signal/signal "Relays"))
 
 (ui/defcomp RelayContent []
-  [ui/label "Relays here"])
+  [ui/column
+   [ui/row
+    [ui/button {} [ui/label "Add Relay"]]
+    [ui/rect {:paint {:fill 0xFFFEFEFE}}
+     [ui/size {:height 30 :width 800}
+      [ui/row
+       ^{:stretch 1}
+       [ui/align {:y :center}
+        [ui/focusable {}
+         [ui/on-key-focused {}
+          [ui/with-cursor {:cursor :ibeam}
+           [ui/text-input {}]]]]]]]]]
+   [ui/gap {:height 10}]
+   [ui/grid {:cols 1}
+    (for [relay ["wss:this" "wss:that" "wss:other"]]
+      [ui/padding
+       {:padding 10}
+       [ui/row
+        [ui/align {:y :center}
+         [ui/label relay]]
+        [ui/button {} "Remove"]]])]])
 
 (ui/defcomp AuthorsContent []
-  [ui/label "Authors here"])
+  [ui/column
+   [ui/row
+    [ui/button {} [ui/label "Add Author npub"]]
+    [ui/rect {:paint {:fill 0xFFFEFEFE}}
+     [ui/size {:height 30 :width 800}
+      [ui/row
+       ^{:stretch 1}
+       [ui/align {:y :center}
+        [ui/focusable {}
+         [ui/on-key-focused {}
+          [ui/with-cursor {:cursor :ibeam}
+           [ui/text-input {}]]]]]]]]]
+   [ui/gap {:height 10}]
+   [ui/grid {:cols 1}
+    (for [npubs ["npub1Jack" "npub1Snowden" "npub1Other"]]
+      [ui/padding
+       {:padding 10}
+       [ui/row
+        [ui/align {:y :center}
+         [ui/label npubs]]
+        [ui/button {} "Remove"]]])]])
 
 (ui/defcomp EventsContent []
-  [ui/label "Events here"])
+  [ui/grid {:cols 1}
+   (for [events ["wss:this" "wss:that" "wss:other"]]
+     [ui/padding
+      {:padding 10}
+      [ui/row
+       [ui/align {:y :center}
+        [ui/label events]]]])])
 
 (ui/defcomp app []
   [ui/column {:width 150}
