0ff5325a03c7bd43858ed2cb1da62553b0553d00
Kyle Grierson <griersonky@gmail.com>
2025-06-03 23:07:19 +0100

    websockets

--- src/nostril/driven/relay.clj
+++ src/nostril/driven/relay.clj
@@ -2,15 +2,12 @@
 
 (ns nostril.driven.relay
   (:require
-   [aleph.http :as http]
    [clojure.pprint :as pprint]
-   [jsonista.core :as json]
    [malli.core :as m]
    [malli.transform :as mt]
-   [manifold.deferred :as deferred]
-   [manifold.stream :as s]
    [nostril.driven.ports :as ports]
-   [nostril.types :as types]))
+   [nostril.types :as types]
+   [nostril.driven.websocket :as websocket]))
 
 (defn request-event
   ([filters] (request-event (random-uuid) filters))
@@ -27,46 +24,24 @@
     "EOSE" (m/decode types/EoseEvent event mt/string-transformer)
     event))
 
-(defrecord InMemoryRelayGateway [stream]
-  ports/RelayGateway
-  (make-connection! [_this _url] stream)
-  (close-connection! [_this _connection])
-  (send! [_this event] (s/put! stream (json/write-value-as-string event))))
-
-(defn make-inmemory-relay-gateway
-  ([] (->InMemoryRelayGateway (s/stream)))
-  ([stream] (->InMemoryRelayGateway stream)))
-
-(defrecord Relay [url stream])
-
-(defrecord AtomRelayManager [relays relay-gateway]
+(defrecord AtomRelayManager [relays]
   ports/RelayManager
   (connect! [_this url]
-    (let [relay-stream (ports/make-connection! relay-gateway url)
-          relay (->Relay url relay-stream)]
-      (swap! relays assoc url relay)))
+    (let [connection (websocket/make-hato-ws url)]
+      (swap! relays assoc url connection)))
   (disconnect! [_this url]
     (let [relay (get @relays url)]
-      (ports/disconnect! relay-gateway (:stream relay))
+      (ports/close-connection! relay)
       (swap! relays dissoc url)))
   (subscribe! [_this url event]
-    (let [relay (get @relays url)
-          stream (:stream relay)
-          [_ subscription-id _] event]
-      (ports/send! relay-gateway event)
-      (deferred/loop []
-        (let [msg (s/take! stream)
-              [event-type :as event] (-> msg
-                                         (json/read-value json/keyword-keys-object-mapper)
-                                         read-event)]
-          (if (= event-type "EOSE")
-            (do
-              (println "EOSE event recieved - closing subscription")
-              (ports/send! relay-gateway (close-event subscription-id))
-              (ports/close-connection! relay-gateway))
-            (do
-              (println event)
-              (deferred/recur))))))))
+    (let [relay (get @relays url)]
+      (ports/send! relay event))))
 
-(defn make-atom-hashmap-relay-manager [relay-gateway]
-  (->AtomRelayManager (atom {}) relay-gateway))
+(defn make-atom-hashmap-relay-manager []
+  (->AtomRelayManager (atom {})))
+
+(comment
+  (let [rm (make-atom-hashmap-relay-manager)
+        url "wss://relay.damus.io"]
+    (ports/connect! rm url)
+    (ports/subscribe! rm url (request-event (random-uuid) {:limit 10}))))
